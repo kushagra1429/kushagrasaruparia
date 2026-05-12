0e0739598925a592396ef744fd579b94c02bf6ae
Kushagra Saruparia <kushagrasaruparia@Kushagras-MacBook-Pro.local>
2026-05-12 23:52:53 +0530

    added dynamic url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const backendUrl = process.env.BACKEND_URL || "http://localhost:5000";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
